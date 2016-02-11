36341f744093e0e69219495f35ce038d1bd17b0b
ryym <ryym.64@gmail.com>
2016-02-11 12:49:35 +0900

    :wrench: Set up test environments

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const gulp   = require('gulp');
 const babel  = require('gulp-babel');
 const del    = require('del');
+const glob   = require('glob');
+const Mocha  = require('mocha');
 
 const GLOB = {
   lib: './lib/**/*.js',
-  build: './build/**/*.js'
+  build: './build/**/*.js',
+  spec: './test/**/*.spec.js'
 };
 
 gulp.task('clean', () => {
@@ -15,4 +18,30 @@ gulp.task('build', ['clean'], () => {
   return gulp.src(GLOB.lib)
     .pipe(babel())
     .pipe(gulp.dest('build/'));
+});
+
+/**
+ * Run tests in the specified file pattern using Mocha
+ */
+function runTests(pattern, options) {
+  const mocha = new Mocha(options);
+  const files = glob.sync(pattern, { realpath: true });
+  files.forEach(file => mocha.addFile(file));
+  return new Promise((resolve, reject) => {
+    try {
+      mocha.run(resolve);
+    } catch(e) {
+      reject(e);
+    }
+  });
+}
+
+gulp.task('test:prepare', () => {
+  require('babel-core/register');
+});
+
+gulp.task('test', ['test:prepare'], () => {
+  return runTests(GLOB.spec)
+    .then(exitCode => process.exit(exitCode))
+    .catch(e => { throw e; });
 });
