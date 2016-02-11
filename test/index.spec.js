945ed04a3e1af32515ef9eacf6055dfc3ea092ca
ryym <ryym.64@gmail.com>
2016-02-11 19:43:09 +0900

    :sunny: Define test cases for all parameters

--- test/index.spec.js
+++ test/index.spec.js
@@ -44,13 +44,43 @@ describe('itEach()', () => {
   });
 
   context('with empty parameters', () => {
-    it('defines no test cases');
+    it('defines no test cases', () => {
+      itEach('', [], () => {}, _it);
+      assert(! _it.calledOnce);
+    });
   });
 
   context('with valid parameters', () => {
-    it('defines the same number of test cases as the parameters');
+    it('defines the same number of test cases as the parameters', () => {
+      const params = [0, 1, 2, 3, 4, 5];
+      itEach('', params, () => {}, _it);
+      assert.equal(_it.callCount, params.length);
+    });
 
-    it('gives each parameter as arguments to the test function');
+    it('gives each parameter as arguments to the test function', () => {
+      const params = [
+        [0, 1, 2],
+        [[2, 4, 6], 40.5],
+        [[1, 2, 3], [4, 5, 6]],
+        ['foo', 'bar'],
+        ['name', { a: 1, b: 1, c: 1 }, {n: []}],
+        [null, undefined, undefined, false]
+      ];
+      const test = sinon.spy();
+      itEach('', params, test, (name, body) => body());
+      assert.deepEqual(test.args, params);
+    });
+
+    context('with an array of non-array values', () => {
+      it('passes each parameter as a first argument', () => {
+        const params = [0, 1, 2, 3];
+        const test = sinon.spy();
+        itEach('', params, test, (name, body) => body());
+
+        const expectedArgs = params.map(p => [p]);
+        assert.deepEqual(test.args, expectedArgs);
+      });
+    });
   });
 
   context('with invalid arguments', () => {
