a5c4bd7651431cfc2dc12afc10567e1a621c0aad
ryym <ryym.64@gmail.com>
2016-02-11 12:56:39 +0900

    :wrench: Configure lint

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const babel  = require('gulp-babel');
 const del    = require('del');
 const glob   = require('glob');
 const Mocha  = require('mocha');
+const eslint = require('eslint');
 
 const GLOB = {
   lib: './lib/**/*.js',
@@ -45,3 +46,44 @@ gulp.task('test', ['test:prepare'], () => {
     .then(exitCode => process.exit(exitCode))
     .catch(e => { throw e; });
 });
+
+/**
+ * Lint the specified files using eslint.
+ */
+function lintFiles(pattern, strict, configs) {
+  const linter = new eslint.CLIEngine(configs);
+  const report = linter.executeOnFiles([pattern]);
+  const formatter = linter.getFormatter();
+  console.log(formatter(report.results));
+  if (0 < report.errorCount || (strict && 0 < report.warningCount)) {
+    throw new Error('eslint reports some problems.');
+  }
+}
+
+gulp.task('lint:lib', () => {
+  lintFiles(GLOB.lib, true);
+});
+
+gulp.task('lint:test', () => {
+  lintFiles(GLOB.spec, true);
+});
+
+gulp.task('lint:gulp', () => {
+  lintFiles('./gulpfile.js', true, {
+    rules: {
+      'no-multi-spaces': 0,
+      'no-console': 0
+    }
+  });
+});
+
+gulp.task('lint', [
+  'lint:lib',
+  'lint:test'
+]);
+
+gulp.task('lint:all', [
+  'lint',
+  'lint:gulp'
+]);
+
